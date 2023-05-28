0197c4dbc730101578880789723360375c908cd8
EduCostam <eduardomc.devs@gmail.com>
2023-05-28 11:47:44 -0300

    Model View Controller

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -1,12 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// controler de produtos
 class Produtos extends CI_Controller {
     public function index() {
+
+		// requisita os dados no model
         $this->load->model("ProdutosModel", "produtos");
 
         $resultado = $this->produtos->listar_todos();
 
+
+		// retorna os dados passando para a view
         $dados = array("produtos" => $resultado);
 
         $this->load->view("produtos/lista", $dados);
